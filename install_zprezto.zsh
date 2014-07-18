abb74b93245574bf4fdc3c52e8192323bdda5efd
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-07-18 05:20:43 -0700

    add comment

--- install_zprezto.zsh
+++ install_zprezto.zsh
@@ -9,6 +9,7 @@ rm -rf \
     ~/.zshenv \
     ~/.zshrc
 
+# Install zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
