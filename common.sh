04fb21a99d62b70ded89ee20577b70f74d5fd96b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-13 03:41:00 -0800

    add ackrc

--- common.sh
+++ common.sh
@@ -70,6 +70,7 @@ rm -rf \
     ~/.tmux.conf \
     ~/.tmux-osx.conf \
     ~/.tmux-nested.conf \
+    ~/.ackrc
 
 # Linking dotfiles
 ln -s $MY_DOTFILES/vim/vimfiles     ~/.vim
@@ -77,6 +78,7 @@ ln -s $MY_DOTFILES/vim/vimrc        ~/.vimrc
 ln -s $MY_DOTFILES/tmux.conf        ~/.tmux.conf
 ln -s $MY_DOTFILES/tmux-osx.conf    ~/.tmux-osx.conf
 ln -s $MY_DOTFILES/tmux-nested.conf ~/.tmux-nested.conf
+ln -s $MY_DOTFILES/ackrc.conf       ~/.ackrc
 echo "vim files symlinked"
 echo "tmux files symlinked"
 
