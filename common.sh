8ffeba41c4514461c6a709f3348cba516ca0334c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-01-14 08:49:57 +0000

    support just the dotfiles, support non 1.9 tmux -- good for beaglebone

--- common.sh
+++ common.sh
@@ -62,32 +62,13 @@ else
   echo "zshell not found, skipping zprezto install" >&2
 fi
 
-
-# Cleaning dotfiles
-rm -rf \
-    ~/.vim \
-    ~/.vimrc \
-    ~/.tmux.conf \
-    ~/.tmux-osx.conf \
-    ~/.tmux-nested.conf \
-    ~/.ackrc
-
-# Linking dotfiles
-ln -s $MY_DOTFILES/vim/vimfiles     ~/.vim
-ln -s $MY_DOTFILES/vim/vimrc        ~/.vimrc
-ln -s $MY_DOTFILES/tmux.conf        ~/.tmux.conf
-ln -s $MY_DOTFILES/tmux-osx.conf    ~/.tmux-osx.conf
-ln -s $MY_DOTFILES/tmux-nested.conf ~/.tmux-nested.conf
-ln -s $MY_DOTFILES/ackrc.conf       ~/.ackrc
-echo "vim files symlinked"
-echo "tmux files symlinked"
-
 # Private Dotfiles
 DropboxDotfileSymlink "ssh"
 DropboxDotfileSymlink "weechat"
 DropboxDotfileSymlink "lolcommits"
 DropboxDotfileSymlink "misc"
 
+source $DOTCONF/dotfiles.sh
 source $DOTCONF/gitconfig.sh
 
 vim +BundleInstall +qall
