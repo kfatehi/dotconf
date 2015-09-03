12033edab487e7638959fd4d587f0b545eba1108
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-09-02 19:49:21 -0700

    set diff submodule mode on

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,5 +1,6 @@
 git config --global core.editor "vim"
 git config --global color.diff true
+git config --global diff.submodule log
 git config --global user.email "keyvanfatehi@gmail.com"
 git config --global user.name "Keyvan Fatehi"
 git config --global push.default matching
