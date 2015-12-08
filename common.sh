dee57209a435a46b91a664f18dfea6f66eb3e480
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-08 12:43:04 -0800

    update tmux confs

--- common.sh
+++ common.sh
@@ -4,10 +4,8 @@ source $DOTCONF/gitconfig.sh
 source $DOTCONF/vim-setup.sh
 
 DotconfSymlink "tmux.conf"
-#DotconfSymlink "tmux-osx.conf"
-#DotconfSymlink "tmux-linux.conf"
-#DotconfSymlink "tmux-nested.conf"
-#DotconfSymlink "tmux-1.9.conf"
+DotconfSymlink "tmux-osx.conf"
+DotconfSymlink "tmux-linux.conf"
 DotconfSymlink "ackrc"
 
 DropboxDotfileSymlink "ssh" && chmod 400 ~/.ssh/id_rsa
