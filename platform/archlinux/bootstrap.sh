1817061011e459d4af816c67eabcb679232dacc7
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-03-15 23:34:15 -0700

    done w archlinux conf

--- platform/archlinux/bootstrap.sh
+++ platform/archlinux/bootstrap.sh
@@ -1,3 +1,15 @@
 source $DOTCONF/common/all.sh
+DotconfSymlink "Xresources"
+DotconfSymlink "Xresources.d"
 
-echo "any arch stuff goes here :)"
+mkdir -p $HOME/.config
+cat <<EOF > $HOME/.config/user-dirs.dirs
+XDG_DESKTOP_DIR="/home/$USER/"
+XDG_DOWNLOAD_DIR="/home/$USER/dl"
+XDG_TEMPLATES_DIR="/home/$USER/"
+XDG_PUBLICSHARE_DIR="/home/$USER/"
+XDG_DOCUMENTS_DIR="/home/$USER/media/docs"
+XDG_MUSIC_DIR="/home/$USER/media/music"
+XDG_PICTURES_DIR="/home/$USER/media/pictures"
+XDG_VIDEOS_DIR="/home/$USER/media/movies"
+EOF
