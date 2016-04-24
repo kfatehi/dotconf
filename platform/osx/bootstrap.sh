3006785df1943a572f3542892e7a5251f3419348
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-04-24 10:31:23 -0700

    bunch of misc stuff

--- platform/osx/bootstrap.sh
+++ platform/osx/bootstrap.sh
@@ -1,3 +1,3 @@
 source $DOTCONF/common/all.sh
 
-brew install tmux vim tree wget nmap ack tmux reattach-to-user-namespace
+brew install tmux vim tree wget nmap ack tmux reattach-to-user-namespace tidy-html5 entr
