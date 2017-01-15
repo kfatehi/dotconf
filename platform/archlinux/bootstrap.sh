bf8a426bba9201c33408b26bea1c004540a62fc1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-01-14 17:49:51 -0800

    cloud link surf bookmarks

--- platform/archlinux/bootstrap.sh
+++ platform/archlinux/bootstrap.sh
@@ -6,16 +6,19 @@ DotconfSymlink "Xresources"
 DotconfSymlink "Xresources.d"
 DotconfSymlink "Xmodmap"
 DotconfSymlink "xinitrc"
+DotconfSymlink "xinitrc"
 
-#mkdir -p $HOME/.config
-#cat <<EOF > $HOME/.config/user-dirs.dirs
-#XDG_DESKTOP_DIR="/home/$USER/"
-#XDG_DOWNLOAD_DIR="/home/$USER/dl"
-#XDG_TEMPLATES_DIR="/home/$USER/"
-#XDG_PUBLICSHARE_DIR="/home/$USER/"
-#XDG_DOCUMENTS_DIR="/home/$USER/media/docs"
-#XDG_MUSIC_DIR="/home/$USER/media/music"
-#XDG_PICTURES_DIR="/home/$USER/media/pictures"
-#XDG_VIDEOS_DIR="/home/$USER/media/movies"
-#EOF
-#
+CloudDotfileSymlink "surf/bookmarks"
+
+mkdir -p $HOME/.config
+cat <<EOF > $HOME/.config/user-dirs.dirs
+XDG_DESKTOP_DIR="/home/$USER/documents"
+XDG_DOWNLOAD_DIR="/home/$USER/dl"
+XDG_TEMPLATES_DIR="/home/$USER/documents/templates"
+XDG_PUBLICSHARE_DIR="/home/$USER/documents/public"
+XDG_DOCUMENTS_DIR="/home/$USER/documents"
+XDG_MUSIC_DIR="/home/$USER/multimedia/music"
+XDG_PICTURES_DIR="/home/$USER/multimedia/pictures"
+XDG_VIDEOS_DIR="/home/$USER/multimedia/movies"
+EOF
+
