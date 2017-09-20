57fed04e1e96981544cf0c28309f690a033b37d8
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-09-19 23:49:32 -0700

    idk stuff

--- bin/letsencrypt.sh
+++ bin/letsencrypt.sh
@@ -1,6 +1,6 @@
 set -x
 ssh gateway -t 'sudo service nginx stop'
-ssh gateway -t 'letsencrypt certonly -d owncloud.keyvan.pw'
+ssh gateway -t 'certbot renew' #letsencrypt certonly -d owncloud.keyvan.pw'
 ssh gateway -t 'sudo service nginx start'
 rsync -L -av gateway:/etc/letsencrypt/live/owncloud.keyvan.pw/ ~/owncloud.keyvan.pw
 rsync -av ~/owncloud.keyvan.pw owncloud:/etc/apache2/ssl
